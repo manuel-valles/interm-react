3e5d08d06491b193d361e6d7de0032b1b5b8e7b9
manukempo <manukempo@gmail.com>
2017-08-29 09:57:22 +0100

    LifeCycle - Mounting & Unmounting

--- pomodoro-react/app/index.js
+++ pomodoro-react/app/index.js
@@ -1,16 +1,39 @@
 class Pomodoro extends React.Component{
+	constructor(){
+		super();
+		this.state={
+			timeElapsed: 0
+		};
+	}
 	totalTime(timeOne, timeTwo){
 		return timeOne + timeTwo;
+	}
+	componentDidMount(){
+		this.interval = setInterval(this.elapseTime.bind(this),1000);
+		this.setState({start: new Date()});
+	}
+	componentWillUnmount(){
+		clearInterval(this.interval);
+	}
+	elapseTime(){
+		//how much time has elapsed?
+		//var timeElapsed = CURRENT TIME - START TIME
+		var timeElapsed = Math.floor((new Date() - this.state.start) / 1000);
+		this.setState({timeElapsed: timeElapsed});
+		// If timeElapsed >= 25 minutes -> then alert!!
+		if(this.state.timeElapsed >= this.props.workingTime*60){
+			clearInterval(this.interval);
+			alert("Time for a break!");
+		}
 	}
 	render(){
 		return(
 			<div>This timer runs from {this.props.workingTime} minutes, 
 			followed by rest of {this.props.restingTime} minutes. 
 			<br/>For a total time of {this.totalTime(this.props.workingTime, this.props.restingTime)} minutes.
-			<br/>There are {} seconds elapsed.</div>
-
+			<br/>There are {this.state.timeElapsed} seconds elapsed.</div>
 		)
 	}
 }
 
-ReactDOM.render(<Pomodoro workingTime={20} restingTime={5}/>, document.getElementById('app'));
+ReactDOM.render(<Pomodoro workingTime={1} restingTime={1}/>, document.getElementById('app'));
--- pomodoro-react/build/index.js
+++ pomodoro-react/build/index.js
@@ -1,25 +1,50 @@
 class Pomodoro extends React.Component {
+	constructor() {
+		super();
+		this.state = {
+			timeElapsed: 0
+		};
+	}
 	totalTime(timeOne, timeTwo) {
 		return timeOne + timeTwo;
 	}
+	componentDidMount() {
+		this.interval = setInterval(this.elapseTime.bind(this), 1000);
+		this.setState({ start: new Date() });
+	}
+	componentWillUnmount() {
+		clearInterval(this.interval);
+	}
+	elapseTime() {
+		//how much time has elapsed?
+		//var timeElapsed = CURRENT TIME - START TIME
+		var timeElapsed = Math.floor((new Date() - this.state.start) / 1000);
+		this.setState({ timeElapsed: timeElapsed });
+		// If timeElapsed >= 25 minutes -> then alert!!
+		if (this.state.timeElapsed >= this.props.workingTime * 60) {
+			clearInterval(this.interval);
+			alert("Time for a break!");
+		}
+	}
 	render() {
 		return React.createElement(
-			'div',
+			"div",
 			null,
-			'This timer runs from ',
+			"This timer runs from ",
 			this.props.workingTime,
-			' minutes, followed by rest of ',
+			" minutes, followed by rest of ",
 			this.props.restingTime,
-			' minutes.',
-			React.createElement('br', null),
-			'For a total time of ',
+			" minutes.",
+			React.createElement("br", null),
+			"For a total time of ",
 			this.totalTime(this.props.workingTime, this.props.restingTime),
-			' minutes.',
-			React.createElement('br', null),
-			'There are ',
-			' seconds elapsed.'
+			" minutes.",
+			React.createElement("br", null),
+			"There are ",
+			this.state.timeElapsed,
+			" seconds elapsed."
 		);
 	}
 }
 
-ReactDOM.render(React.createElement(Pomodoro, { workingTime: 20, restingTime: 5 }), document.getElementById('app'));
+ReactDOM.render(React.createElement(Pomodoro, { workingTime: 1, restingTime: 1 }), document.getElementById('app'));
