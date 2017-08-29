521618885e41bf0cdc4402217be0945ac7ad4706
manukempo <manukempo@gmail.com>
2017-08-29 17:02:24 +0100

    Pass the query

--- product-search-react/app/index.js
+++ product-search-react/app/index.js
@@ -1,10 +1,33 @@
 var React = require('react');
 var ReactDOM = require('react-dom');
+var products = require('./products.js');
+
+class Results extends React.Component{
+	componentWillReceiveProps(nextProps){
+		console.log(nextProps.products);
+	}
+	render(){
+		return(
+			<div className="results">
+				<div className="in-stock">
+					<h2><a href="#">Toothpaste</a></h2>
+					<p>$2.99</p>
+					<p>Lorem Ipsum is simply dummy text of the printing and 
+					typesetting industry. Lorem Ipsum has been the industry's 
+					standard dummy text ever since the 1500s, when an unknown 
+					printer took a galley of type and scrambled it to make a type 
+					specimen book.</p>
+				</div>
+			
+			</div>
+		)
+	}
+}
 
 class SearchBar extends React.Component{
 	
 	handleQuery(e){
-		console.log(e.target.value);
+		this.props.onQuery(e.target.value);
 	}
 	render(){
 		return(
@@ -16,25 +39,24 @@ class SearchBar extends React.Component{
 }
 
 class Search extends React.Component{
+	constructor(props){
+		super(props);
+		this.state={
+			query:''
+		};
+	}
+	handleQuery(query){
+		this.setState({'query': query.toLowerCase().trim()})
+	}
 	render(){
 		return(
 			<div className="search">
-				<SearchBar />
-				<div className="results">
-					<div className="in-stock">
-						<h2><a href="#">Toothpaste</a></h2>
-						<p>$2.99</p>
-						<p>Lorem Ipsum is simply dummy text of the printing and 
-						typesetting industry. Lorem Ipsum has been the industry's 
-						standard dummy text ever since the 1500s, when an unknown 
-						printer took a galley of type and scrambled it to make a type 
-						specimen book.</p>
-					</div>
+				<SearchBar onQuery={this.handleQuery.bind(this)}/>
+				<Results products={this.props.products} query={this.state.query}/>
 				
-				</div>
 			</div>
 		);
 	}
 }
 
-ReactDOM.render(<Search />, document.getElementById('app'));
+ReactDOM.render(<Search products={products}/>, document.getElementById('app'));
