e1c46c642921f56a83e427be43a8b27ee78fd280
manukempo <manukempo@gmail.com>
2017-08-29 17:48:32 +0100

    Show each product result

--- product-search-react/app/index.js
+++ product-search-react/app/index.js
@@ -2,23 +2,43 @@ var React = require('react');
 var ReactDOM = require('react-dom');
 var products = require('./products.js');
 
+class Result extends React.Component{
+	displayPrice(price){
+		return price / 100.00;
+	}
+	render(){
+		return(
+			<div className="in-stock">
+				<h2><a href="#">{this.props.product.name}</a></h2>
+				<p>${this.displayPrice(this.props.product.price)}</p>
+				<p>{this.props.product.description}</p>
+			</div>
+		)
+	}
+}
+
 class Results extends React.Component{
+	constructor(props){
+		super(props);
+		this.state={
+			foundProducts: props.products
+		}
+	}
 	componentWillReceiveProps(nextProps){
-		console.log(nextProps.products);
+		var foundProducts= nextProps.products.filter(product =>{
+			return 	product.name.toLowerCase().match(nextProps.query.toLowerCase())||
+					product.description.toLowerCase().match(nextProps.query.toLowerCase());
+		});
+		this.setState({
+			foundProducts: foundProducts
+		});
 	}
 	render(){
 		return(
 			<div className="results">
-				<div className="in-stock">
-					<h2><a href="#">Toothpaste</a></h2>
-					<p>$2.99</p>
-					<p>Lorem Ipsum is simply dummy text of the printing and 
-					typesetting industry. Lorem Ipsum has been the industry's 
-					standard dummy text ever since the 1500s, when an unknown 
-					printer took a galley of type and scrambled it to make a type 
-					specimen book.</p>
-				</div>
-			
+				{this.state.foundProducts.map((product,i)=>
+					<Result product={product} key={i}/>)
+				}
 			</div>
 		)
 	}
