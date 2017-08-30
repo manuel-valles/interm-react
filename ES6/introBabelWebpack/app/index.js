1fbd1f95c920f26b1eb2a4effd38143ea72e8a4a
manukempo <manukempo@gmail.com>
2017-08-30 16:37:37 +0100

    ES6 Essentials1

--- ES6/introBabelWebpack/app/index.js
+++ ES6/introBabelWebpack/app/index.js
@@ -1,1 +1,73 @@
-console.log("Hello World form Webpack!!, with an automatic update too!");
+// ASSIGMENT
+// Let
+let limit = 100;
+limit=200;
+console.log(limit);
+// Const
+const limit2 = 120;
+// limit2 +=100;7  //This gives us a read-only error
+console.log(limit2);
+
+const emails = ['frodo@email.com', 'samwise@email.com', 'marry@email.com'];
+// emails = []; //This gives us a read-only error
+emails.push('pippin@email.com'); //Otherwise this will run fine.
+console.log(emails);
+
+// BLOCK SCOPING
+// Let - Const has the same behavior here, since it's global/local scope
+let limit3 = 200;
+{
+	let limit3 = 10;
+	console.log('backstage limit=', limit3);
+}
+console.log('overal venue limit=', limit3);
+
+// Var
+var limit4 = 200;
+{
+	var limit4 = 10;
+	console.log('backstage limit=', limit4);
+}
+console.log('overal venue limit=', limit4);
+
+// TEMPLATE LITERAL
+let a=`good`;
+let greeting =`${a} morning`;
+console.log(a);
+console.log(greeting);
+
+// SPREAD OPERATOR
+let b=[20,30,40];
+let c=[10,...b,50];
+console.log(c);
+
+let d=['Dana', 'Erik', 'Frank'];
+let e=['Alice', 'Bob', 'Carl', ...d];
+console.log(e);
+
+function collect(...a){
+	console.log(a);
+}
+collect(1,2,3,4,5);
+
+// DESTRUCTING ASSIGNMENTS 
+let f = [4,5,6];
+/*let four = f[0];
+let five = f[1];*/
+let [four, five] = f;
+console.log(four, five);
+
+let animals = ['Simba', 'Zazu', 'Ed'];
+let [lion, bird] = animals;
+console.log(lion, bird);
+
+let king = {name: 'Mufasa', kids:1};
+/*let name = king.name;
+let kids = king.kids;*/
+let {name, kids}= king;
+console.log(name, kids);
+
+let son = {name2: 'Simba', parents:2};
+let name2, parents;
+({name2, parents} = son);
+console.log(name2, parents);
